832c1ea7bfd378709c6e2d93aa7e5cef27720655
Galesy <heitor@estevam.org>
2024-12-19 13:51:43 -0300

    campfire working!

--- scenes/enemy/nav_enemy.gd
+++ scenes/enemy/nav_enemy.gd
@@ -21,8 +21,6 @@ const JUMP_VELOCITY = -700.0
 func _physics_process(delta: float) -> void:
 	if navigation_agent_2d.distance_to_target() < 10.0 and campfire_attack.is_stopped():
 		campfire_attack.start()
-	else:
-		campfire_attack.stop()
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
