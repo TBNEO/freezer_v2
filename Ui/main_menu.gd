500cbeb00837c5600afb9eb7b2822a57ddb47814
Galesy <heitor@estevam.org>
2024-12-20 15:42:15 -0300

    pause menu fix

--- Ui/main_menu.gd
+++ Ui/main_menu.gd
@@ -1,16 +1,21 @@
 extends Control
 
 @onready var audio = $AudioStreamPlayer2D
+@onready var settings = $settings
 
 func _on_start_button_down():
 	audio.play()
 	get_tree().change_scene_to_file("res://scenes/MAIN.tscn")
 
 func _on_options_button_down():
-	##do this eventually
+	settings.show()
 	audio.play()
 	pass
 
 func _on_quit_button_down():
 	audio.play()
 	get_tree().quit()
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("pause"):
+		settings.hide()
--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -1,14 +1,29 @@
 extends Control
 
 signal resume
+signal paused
+@onready var settings = $settings
 
 func _on_resume_button_down():
 	resume.emit()
-
+	print("grah")
 
 func _on_options_button_down():
-	pass # Replace with function body.
-
+	settings.show()
+	settings.mouse_filter = MOUSE_FILTER_STOP
 
 func _on_exit_to_title_button_down():
 	get_tree().change_scene_to_file("res://Ui/main_menu.tscn")
+
+func _process(delta):
+	if !visible:
+		if Input.is_action_just_pressed("pause"):
+			paused.emit()
+		else:
+			return
+	else:
+		if Input.is_action_just_pressed("pause") and settings.visible:
+			settings.hide()
+			settings.mouse_filter = MOUSE_FILTER_PASS
+		elif Input.is_action_just_pressed("pause") and !settings.visible:
+			resume.emit()
